10b1e5ab532c3c3875c02cd489076b633e741abe
elishakstevens <elisha.stevens@gmail.com>
2024-12-29 17:04:57 -0800

    Update Repo - Add program

---
+++ Acme-program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string acme = "Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!";
+        Console.WriteLine(acme);
+        Console.ReadLine();
+    }
+}
