84161e6be20526a398ac58d5adcd8e0285b317d4
elishakstevens <elisha.stevens@gmail.com>
2025-01-11 23:46:09 -0800

    Create Math Operations Program

---
+++ MathOpsProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOpsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for a number and save that number
+            Console.WriteLine("What number do you want to do math operations on?");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //Instantiating class w/ user input
+            Operations operations = new Operations();
+
+            //adding user input 
+            int answerAdd = operations.Add(userNum);
+            Console.WriteLine(userNum + " plus 15 equals " + answerAdd);
+            Console.ReadLine();
+
+            //multiplying user input
+            int answerMul = operations.Multiply(userNum);
+            Console.WriteLine(userNum + " times 15 equals " + answerMul);
+            Console.ReadLine();
+
+            //subtracting user input
+            int answerSub = operations.Subtract(userNum);
+            Console.WriteLine(userNum + " minus 15 equals " + answerSub);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperationsClass(MathOpsProgram).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOpsProgram
+{
+    public class Operations
+    {
+        //Creating methods for Operations class
+        public int Add(int num)
+        {
+            int number = num + 15;
+            return number;
+        }
+
+        public int Multiply(int num)
+        {
+            int number = num * 15;
+            return number;
+        }
+
+        public int Subtract(int num)
+        {
+            int number = num - 15;
+            return number;
+        }
+    }
+}
