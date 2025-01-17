514068208eeeced212efdd175240fe3b64cb4d00
elishakstevens <elisha.stevens@gmail.com>
2025-01-16 21:58:04 -0800

    Creating Math Operations Program
    Creating different methods, all with the same name

---
+++ MathOperationsProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate first method of Operations class and display answer to console
+            Operations addition = new Operations();
+            addition = Operations.OperationOne(addition, 43);
+            Console.WriteLine("The answer to {0} plus {1} equals {2}.", addition.Number, addition.Number, addition.Answer);
+            Console.ReadLine();
+
+            //Instantiate second method of Operations class and display answer to console
+            Operations multiply = new Operations();
+            multiply = Operations.OperationOne(multiply, 34.68);
+            Console.WriteLine("The answer to {0} times {1} equals about {2}.", multiply.Decimal, multiply.Decimal, multiply.Answer);
+            Console.ReadLine();
+
+            //Instantiate third method of Operations class and display answer to console
+            Operations divide = new Operations();
+            divide = Operations.OperationOne(divide, "943");
+            //Only display to console if method does not return null
+            if (null != divide)
+            {
+                Console.WriteLine("The answer to {0} divided by 3 equals {1}.", divide.Number, divide.Answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Operations(MathOperationsProgram).cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    internal class Operations
+    {
+        //Create class and set all properties to 0
+        public Operations()
+        {
+            Number = 0;
+            Decimal = 0.0;
+            Answer = 0;
+        }
+        //Defining each property's data type
+        public int Number { get; set; }
+        public double Decimal { get; set; }
+        public int Answer { get; set; }
+
+        //First method of class, adding an integar to itself
+        public static Operations OperationOne(Operations addition, int num)
+        {
+            addition.Number = num;
+            addition.Decimal = 0;
+            addition.Answer = num + num;
+            return addition;
+        }
+
+        //Second method of class, multiplying a decimal number to itself and returning an integar
+        public static Operations OperationOne(Operations multiply, double number)
+        {
+            double answer = number * number;
+            int ansInt = Convert.ToInt32(answer);
+            multiply.Number = 0;
+            multiply.Decimal = number;
+            multiply.Answer = ansInt;
+            return multiply;
+        }
+
+        //Third method of class, converting number as a string to an integar and dividing by 3
+        //If string provided is not a number, give error message, return null and close program
+        public static Operations OperationOne(Operations divide, string divideNum)
+        {
+            bool numBool = int.TryParse(divideNum, out _);
+            if (!numBool)
+            {
+                Console.WriteLine("Error detected. Goodbye.");
+                Console.ReadLine();
+            }
+            else if (numBool)
+            {
+                int divideNumber = Convert.ToInt32(divideNum);
+                divide.Decimal = 0;
+                divide.Number = divideNumber;
+                divide.Answer = divideNumber/3;
+                return divide;
+            }
+            return null;
+        }
+
+    }
+}
