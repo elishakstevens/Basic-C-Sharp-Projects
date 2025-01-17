b041e31537192e93b64b98b8ae54cd6e411cda74
elishakstevens <elisha.stevens@gmail.com>
2025-01-16 22:32:26 -0800

    Creating Method Program
    Optional parameters

---
+++ MethodProgram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Operations class
+            Operations pemdas = new Operations();
+
+            //Prompt user to provide a number
+            Console.WriteLine("Please provide a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //Prompt user to provide a second number, if they want
+            Console.WriteLine("Please provide a second number:\nThis is optional. Press Enter to skip.");
+            string optionalNum = Console.ReadLine();
+
+            //If user does not provide a second number, use the pre-defined second number in the pemdas method
+            //Display answer on the console
+            if (string.IsNullOrEmpty(optionalNum))
+            {
+                pemdas = Operations.Operation(pemdas, userNum);
+                Console.WriteLine("With the number you provided, the answer is {0}.", pemdas.Answer);
+            }
+            //If user does provide a second number, it will override the pre-defined number
+            //Display answer on the console
+            else
+            {
+                int userNumTwo = Convert.ToInt32(optionalNum);
+                pemdas = Operations.Operation(pemdas, userNum, userNumTwo);
+                Console.WriteLine("With the numbers you provided, the answer is {0}.", pemdas.Answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Operations(MethodProgram).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Operations
+    {
+        //Create class and set properties to 0
+        public Operations()
+        {
+            Number = 0;
+            Answer = 0;
+        }
+        //Define each property's data types
+        public int Number { get; set; }
+        public int Answer { get; set; }
+
+        //Create method for class where two numbers are used for a math operation and the answer is returned
+        //The first number is required but the second number is optional
+        public static Operations Operation(Operations pemdas, int number, int numTwo = 2)
+        {
+            int answer = ((number + numTwo * 7) / 2) - number;
+            pemdas.Number = number;
+            pemdas.Answer = answer;
+            return pemdas;
+        }
+    }
+}
