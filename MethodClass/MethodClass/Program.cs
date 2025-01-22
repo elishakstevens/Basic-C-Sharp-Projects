120fca0f0b010133744013ecd901801cd43819ef
elishakstevens <elisha.stevens@gmail.com>
2025-01-21 21:46:52 -0800

    Update and Simplify Method Class Program

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate OpVoid class
+            OpVoid operation = new OpVoid();
+
+            //Call method and display results to the console
+            operation.Operation(operation, 45, 31);
+            Console.WriteLine("\n\nOperation with first number: " + operation.Answer + "\n\nSecond Number: " + operation.NumberTwo + "\n");
+            Console.ReadLine();
+
+            //Call method, specifying parameters by name, and display results to the console
+            operation.Operation(operation, numOne: 92, numTwo: 94);
+            Console.WriteLine("\n\nOperation with first number: " + operation.Answer + "\n\nSecond Number: " + operation.NumberTwo + "\n");
+            Console.ReadLine();
+        }
+    }
+}
--- MethodClassProgram.cs
+++ MethodClassProgram.cs
@@ -8,20 +8,23 @@ namespace MethodClass
 {
     internal class Program
     {
+        static void Operation(int num1, int num2)
+        {
+            int num3 = (num1 * 3) % 6;
+            Console.WriteLine("Second Integar: " + num2);
+            Console.ReadLine();
+            Console.WriteLine("Answer to Operation on First Integar: " + num3);
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            //Instantiate OpVoid class
-            OpVoid operation = new OpVoid();
-
-            //Call method and display results to the console
-            operation.Operation(operation, 45, 31);
-            Console.WriteLine("\n\nOperation with first number: " + operation.Answer + "\n\nSecond Number: " + operation.NumberTwo + "\n");
-            Console.ReadLine();
-
-            //Call method, specifying parameters by name, and display results to the console
-            operation.Operation(operation, numOne: 92, numTwo: 94);
-            Console.WriteLine("\n\nOperation with first number: " + operation.Answer + "\n\nSecond Number: " + operation.NumberTwo + "\n");
-            Console.ReadLine();
+            //Instantiate Class
+            var operation = new Program();
+            //Call method, passing in two numbers
+            Operation(45, 31);
+            //Call method, specifying parameters by name
+            Operation(num1: 92, num2: 94);
         }
     }
 }
