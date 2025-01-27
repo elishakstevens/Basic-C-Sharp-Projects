9de49a688b6fb367711489498fa4099fcd98f129
elishakstevens <elisha.stevens@gmail.com>
2025-01-26 16:56:02 -0800

    Overloading Comparison Operators

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    public class Employee
+    {
+        //Properties of Class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Overloading '==' operator
+        public static bool operator ==(Employee employeeOne, Employee employeeTwo)
+        {
+            return (employeeOne.Id == employeeTwo.Id);
+        }
+
+        //Overloading '!=' operator
+        public static bool operator !=(Employee employeeOne, Employee employeeTwo)
+        {
+            return (employeeOne.Id != employeeTwo.Id);
+        }
+     }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate two Employee objects
+            Employee employeeOne = new Employee() { Id = 123, FirstName = "Elisha", LastName = "Stevens" };
+            Employee employeeTwo = new Employee() { Id = 123, FirstName = "Elijah", LastName = "Stevens"};
+
+            //Full Name variables to make things easier
+            string NameOne = employeeOne.FirstName + " " + employeeOne.LastName;
+            string NameTwo = employeeTwo.FirstName + " " + employeeTwo.LastName;
+
+            //Comparing both objects and displaying a message on the console
+            if (employeeOne == employeeTwo)
+            {
+                Console.WriteLine("{0} and {1} have the same ID.", NameOne, NameTwo);
+            }
+            else if (employeeOne != employeeTwo)
+            {
+                Console.WriteLine("{0} and {1} have different IDs.", NameOne, NameTwo);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
