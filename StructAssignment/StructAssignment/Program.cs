00a3cfaaf426e16f02c80b93abb44a660dff9e1b
elishakstevens <elisha.stevens@gmail.com>
2025-01-30 20:55:05 -0800

    Creating and instantiating a struct

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate struct and assign amount to prop
+            Number number = new Number();
+            number.Amount = 46;
+
+            //Display amount on console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
