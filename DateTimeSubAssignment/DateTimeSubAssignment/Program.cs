a088b9d71d1b6de3823e423d035c0c12b602273f
elishakstevens <elisha.stevens@gmail.com>
2025-02-02 21:11:02 -0800

    Create DateTime Program
    Adding hours to current date and time to see what date and time would change to

---
+++ DateTimeSubAssignment/DateTimeSubAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a DateTime object of the current date and time
+            //Display current to console
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(DateTime.Now);
+
+            //Prompt user to provide a number of hours
+            //Convert and save number
+            Console.WriteLine("\nPlease provide a number of hours: ");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            //Use AddHours method from DateTime to add hours provided by user to the current date and time
+            //Instantiate a new DateTime Object of this new System.DateTime
+            //Display number of hours and the new date and time to console
+            DateTime newDateTime = dateTime.AddHours(hours);
+            Console.WriteLine("\nIn {0} hours, it will be this date and time:\n{1}", hours, newDateTime);
+            Console.ReadLine();   
+        }
+    }
+}
