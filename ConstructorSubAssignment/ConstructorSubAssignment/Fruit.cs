209ffac7e127e3c0a0d2827bdb9d4e7f4edd7397
elishakstevens <elisha.stevens@gmail.com>
2025-02-08 17:17:35 -0800

    Chaining Constructors

---
+++ ConstructorSubAssignment/ConstructorSubAssignment/Fruit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubAssignment
+{
+    public class Fruit
+    {
+        //Setting private properties that won't be needed
+        private string name;
+        private string descriptor;
+        
+        //Chaining two constructors if user does not provide descriptor
+        public Fruit(string name) : this(name, "yummy")
+        {
+        }
+        //Creating constructor
+        public Fruit(string name, string descriptor)
+        {
+            this.name = name;
+            this.descriptor = descriptor;
+            Console.WriteLine("A {0} {1} is my favorite fruit too!", descriptor, name);
+        }
+    }
+}
---
+++ ConstructorSubAssignment/ConstructorSubAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setting a constant variable that cannot be changed
+            const string FoodType = "fruit";
+
+            //Prompting user and saving response
+            Console.WriteLine("What is your favorite {0}?", FoodType);
+            string favFruit = Console.ReadLine().ToLower();
+
+            //Returning text to user based on response
+            Console.WriteLine("Your favorite {0} is {1}!", FoodType, favFruit);
+            Console.ReadLine();
+
+            //Promping user and saving reponse
+            Console.WriteLine("How would you describe a {0} in one word?\n(Press enter to skip.)", favFruit);
+            string fruitWord = Console.ReadLine().ToLower();
+
+            //If user does not provide a descriptor, will provide default
+            //If user does, will use what they provide
+            if (fruitWord == "")
+            {
+                var fruit = new Fruit(favFruit);
+            }
+            else
+            {
+                var fruit = new Fruit(favFruit, fruitWord);
+            }
+            Console.ReadLine();
+        }
+    }
+}
